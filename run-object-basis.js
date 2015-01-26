4d7fdd7be030d29f43fbb4c98a060e09b0202c07
David Björklund <david.bjorklund@gmail.com>
2015-01-26 23:44:25 +0100

    DRY

--- run-object-basis.js
+++ run-object-basis.js
@@ -7,32 +7,28 @@ module.exports = function (base) {
         var result = is.array(object) ? [] : {}
           , tasks = []
 
+          , put = function (key, done) {
+              return function (err, value) {
+                if (err) return done(err)
+                result[key] = value
+                done()
+              }
+            }
+
         Object.keys(object).forEach(function (key) {
           var value = object[key]
 
           if (isStream (value)) {
             tasks.push(function (done) {
-              collect(value, function (err, value2) {
-                if (err) return done(err)
-                result[key] = value2
-                done(null)
-              })
+              collect(value, put(key, done))
             })
           } else if (is.function (value))
             tasks.push(function (done) {
-              value(function (err, value2) {
-                if (err) return done(err)
-                result[key] = value2
-                done(null)
-              })
+              value(put(key, done))
             })
           else if (is.object (value))
             tasks.push(function (done) {
-              run(value, function (err, value2) {
-                if (err) return done(err)
-                result[key] = value2
-                done(null)
-              })
+              run(value, put(key, done))
             })
           else
             result[key] = object[key]
